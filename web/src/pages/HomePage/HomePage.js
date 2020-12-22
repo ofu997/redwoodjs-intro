e6197f488e68c9b0c29b6c7b59f830bdb1404971
oliver fu <ofu997@gmail.com>
2020-12-22 14:26:06 -0800

    add home page and about page using a layout and redwood Link

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,8 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const HomePage = () => {
+  return <BlogLayout children='Home'/ >
+}
+
+export default HomePage
